88325261a31e34253dec1da8a57d66a5a03c66a4
FathimaMehrinVS <mehrinfiza57@gmail.com>
2026-07-12 17:35:35 +0530

    Fix eslint JSX unused variable warnings

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -2,6 +2,7 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import react from 'eslint-plugin-react'
 
 export default [
   { ignores: ['dist'] },
@@ -19,14 +20,18 @@ export default [
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      'react': react,
     },
     rules: {
       ...js.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
+      'react/jsx-uses-react': 'error',
+      'react/jsx-uses-vars': 'error',
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
+      'no-unused-vars': 'warn', // Change unused vars from red error to yellow warning
     },
   },
 ]
